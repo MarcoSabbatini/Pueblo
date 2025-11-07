e4dae9ae19944ca13a36caa75839f3b17e0ec0f1
MarcoSabbatini <117950317+MarcoSabbatini@users.noreply.github.com>
2025-11-07 09:21:20 +0100

    button is used for each axis rotation, also val btn works

--- single_cube_view.py
+++ single_cube_view.py
@@ -52,25 +52,6 @@ def update_cubes(ax, vertices_list, colors):
     ax.set_zlim([-1, 5])
     plt.draw()
 
-"""def rotation_matrix_x(angle):
-    c, s = np.cos(angle), np.sin(angle)
-    return np.array([[1, 0, 0], [0, c, -s], [0, s, c]])
-
-def rotation_matrix_y(angle):
-    c, s = np.cos(angle), np.sin(angle)
-    return np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
-
-def rotation_matrix_z(angle):
-    c, s = np.cos(angle), np.sin(angle)
-    return np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]])
-
-
-#gets the avg of the whole cube
-def get_global_center(vertices_list):
-    all_points = np.vstack(vertices_list) # puts one on top of the other the rows, impiles one over each other, they have to be the same dim
-    return np.mean(all_points, axis=0)
-"""
-
 #related to center
 def get_vertices_from_positions(cube_positions, center):
     vertices_list = []
@@ -79,8 +60,15 @@ def get_vertices_from_positions(cube_positions, center):
         vertices_list.append(create_unit_cube(offset))
     return vertices_list
 
-def translate_center(center, tx, ty, tz):
-    return center + np.array([tx, ty, tz])
+def translate_center(center, direction):
+    return center + direction
+
+#vertices updated globally
+def translate(ax, direction):
+    global center, cube_positions, colors, global_vertices_list
+    center = translate_center(center, direction)
+    global_vertices_list = get_vertices_from_positions(cube_positions, center)
+    update_cubes(ax, global_vertices_list, colors)
 
 #k may be used better, rotates by 90 around x/y/z axis passing in the center
 def rotate_positions(cube_positions, axis, k=1):
@@ -104,88 +92,52 @@ def rotate_positions(cube_positions, axis, k=1):
     else:
         return
 
-    # scala interna per gestire mezze unità: moltiplichiamo per 2
     scale = 2
-    pos2 = cube_positions * scale              # ora sono interi (multipli di 2)
+    pos2 = cube_positions * scale              
     min2 = pos2.min(axis=0)
     max2 = pos2.max(axis=0)
 
-    # centro della bounding box in scala 2: (min2 + max2) / 2 -> sempre intero
-    center2 = (min2 + max2) // 2               # intero preciso nello spazio scalato
+    center2 = (min2 + max2) // 2               
 
-    # trasliamo al centro, ruotiamo k volte, ritrasliamo
-    relative2 = pos2 - center2                 # valori interi
+    relative2 = pos2 - center2                 
     for _ in range(k % 4):
-        # rotation in the doubled lattice: R maps integers->integers
-        relative2 = (relative2 @ R.T)          # row-vector convention
+        relative2 = (relative2 @ R.T)          
     pos2_rot = relative2 + center2
 
-    # torniamo alla scala originale dividendo per 2
-    # pos2_rot deve essere multiplo di 2 se tutto è coerente -> divisione esatta
     return (pos2_rot // scale).astype(int)
-
-#rotation function on click
-"""def rotate(event, axis, angle, ax, colors):
-    global global_vertices_list, current_rotation_matrix
-    if axis == 'x':
-        R = rotation_matrix_x(angle)
-    elif axis == 'y':
-        R = rotation_matrix_y(angle)
-    elif axis == 'z':
-        R = rotation_matrix_z(angle)
-    else:
-        return
-    
-    center = get_global_center(global_vertices_list)
-
-    #rotates cubelets around center
-    new_vertices = []
-    for v in global_vertices_list:
-        rotated = np.dot(v - center, R.T) + center
-        new_vertices.append(rotated)
-
-    global_vertices_list = new_vertices
-    update_cubes(ax, global_vertices_list, colors)
-"""
 
 #cube translate adding what's insiede the textboxes
 def update_position(ax, event=None):
-    global center
+    global center, global_vertices_list
     try:
         tx = float(text_box_x.text)
         ty = float(text_box_y.text)
         tz = float(text_box_z.text)
-        #translation = np.array([tx, ty, tz])
-
-        center = translate_center(center, tx, ty, tz)
-        global_vertices_list = get_vertices_from_positions(cube_positions, center)
+        direction = np.array([tx, ty, tz], dtype=float)
+        center = translate_center(center, direction)
+        global_vertices_list = get_vertices_from_positions(cube_positions, center) #passes vector with translation info
         update_cubes(ax, global_vertices_list, colors)
     except ValueError:
         pass  
 
 def rotate(event, axis, ax, colors):
-    global cube_positions, center
+    global cube_positions, center, global_vertices_list
     cube_positions = rotate_positions(cube_positions, axis, k=1)
-    vertices_list = get_vertices_from_positions(cube_positions, center)
-    update_cubes(ax, vertices_list, colors)
+    global_vertices_list = get_vertices_from_positions(cube_positions, center) # save vertices globally after rot
+    update_cubes(ax, global_vertices_list, colors)
 
 def validate_positions(ax):
-    global global_vertices_list
-
-    invalid_cubes = []
-    for i, vertices in enumerate(global_vertices_list):
+    current_vertices =get_vertices_from_positions(cube_positions, center)
+    invalid_found = False
+    for i, vertices in enumerate(current_vertices):
         if np.any(vertices[:, 2] < 0):
-            invalid_cubes.append(i)
+            invalid_found = True
+            break
 
-    if invalid_cubes:
-        print(f"not ok")
-        temp_colors = colors.copy()
-        for idx in invalid_cubes:
-            temp_colors[idx] = 'red'
-        update_cubes(ax, global_vertices_list, temp_colors)
+    if invalid_found:
+        print("not ok")
     else:
         print("ok")
-        update_cubes(ax, global_vertices_list, colors)
 
 
 def main():
@@ -203,36 +155,48 @@ def main():
 
     update_cubes(ax, global_vertices_list, colors)
 
-    ax_box_x = plt.axes([0.2, 0.15, 0.1, 0.05])
-    text_box_x = TextBox(ax_box_x, 'X', initial="0")
+    # translation buttons
+    ax_btn_px = plt.axes([0.45, 0.01, 0.06, 0.075])
+    btn_px = Button(ax_btn_px, '+X')
+    btn_px.on_clicked(lambda event: translate(ax, np.array([1, 0, 0])))
 
-    ax_box_y = plt.axes([0.35, 0.15, 0.1, 0.05])
-    text_box_y = TextBox(ax_box_y, 'Y', initial="0")
+    ax_btn_mx = plt.axes([0.52, 0.01, 0.06, 0.075])
+    btn_mx = Button(ax_btn_mx, '-X')
+    btn_mx.on_clicked(lambda event: translate(ax, np.array([-1, 0, 0])))
 
-    ax_box_z = plt.axes([0.5, 0.15, 0.1, 0.05])
-    text_box_z = TextBox(ax_box_z, 'Z', initial="0")
+    ax_btn_py = plt.axes([0.59, 0.01, 0.06, 0.075])
+    btn_py = Button(ax_btn_py, '+Y')
+    btn_py.on_clicked(lambda event: translate(ax, np.array([0, 1, 0])))
 
-    text_box_x.on_text_change(lambda val: update_position(ax))
-    text_box_y.on_text_change(lambda val: update_position(ax))
-    text_box_z.on_text_change(lambda val: update_position(ax))
+    ax_btn_my = plt.axes([0.66, 0.01, 0.06, 0.075])
+    btn_my = Button(ax_btn_my, '-Y')
+    btn_my.on_clicked(lambda event: translate(ax, np.array([0, -1, 0])))
+
+    ax_btn_pz = plt.axes([0.73, 0.01, 0.06, 0.075])
+    btn_pz = Button(ax_btn_pz, '+Z')
+    btn_pz.on_clicked(lambda event: translate(ax, np.array([0, 0, 1])))
+
+    ax_btn_mz = plt.axes([0.80, 0.01, 0.06, 0.075])
+    btn_mz = Button(ax_btn_mz, '-Z')
+    btn_mz.on_clicked(lambda event: translate(ax, np.array([0, 0, -1])))
 
     #buttons for rotation
-    ax_rot_x_90 = plt.axes([0.07, 0.01, 0.1, 0.075])
+    ax_rot_x_90 = plt.axes([0.05, 0.01, 0.1, 0.075])
     btn_rot_x_90 = Button(ax_rot_x_90, 'Rotate X 90')
 
-    ax_rot_y_90 = plt.axes([0.33, 0.01, 0.1, 0.075])
+    ax_rot_y_90 = plt.axes([0.18, 0.01, 0.1, 0.075])
     btn_rot_y_90 = Button(ax_rot_y_90, 'Rotate Y 90')
 
-    ax_rot_z_90 = plt.axes([0.60, 0.01, 0.1, 0.075])
+    ax_rot_z_90 = plt.axes([0.31, 0.01, 0.1, 0.075])
     btn_rot_z_90 = Button(ax_rot_z_90, 'Rotate Z 90')
 
-    ax_val_cube = plt.axes([0.85, 0.01, 0.1, 0.075])
+    ax_val_cube = plt.axes([0.88, 0.01, 0.1, 0.075])
     btn_val_cube = Button(ax_val_cube, "Check rot/pos")
 
     btn_rot_x_90.on_clicked(lambda event: rotate(event, 'x', ax, colors))
     btn_rot_y_90.on_clicked(lambda event: rotate(event, 'y', ax, colors))
     btn_rot_z_90.on_clicked(lambda event: rotate(event, 'z', ax, colors))
-    btn_val_cube.on_clicked(lambda event: validate_positions(ax, cube_positions, center, colors))
+    btn_val_cube.on_clicked(lambda event: validate_positions(ax))
 
 
     plt.show()
